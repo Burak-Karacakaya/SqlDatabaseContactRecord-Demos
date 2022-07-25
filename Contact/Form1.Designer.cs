75d0d15b06cd5d57f0ecef0c3e7e5ee3e2f00eac
burak karacakaya <burakaracakaya@gmail.com>
2022-07-25 15:25:57 +0300

    Add project files.

---
+++ Contact/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Contact
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Contact = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.newContact = new System.Windows.Forms.GroupBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.mskPhonecellNumber = new System.Windows.Forms.MaskedTextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.txtMailAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.Contact.SuspendLayout();
+            this.newContact.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Contact
+            // 
+            this.Contact.Controls.Add(this.dataGridView1);
+            this.Contact.Location = new System.Drawing.Point(12, 7);
+            this.Contact.Name = "Contact";
+            this.Contact.Size = new System.Drawing.Size(342, 245);
+            this.Contact.TabIndex = 30;
+            this.Contact.TabStop = false;
+            this.Contact.Text = "Contact";
+            // 
+            // newContact
+            // 
+            this.newContact.Controls.Add(this.btnClear);
+            this.newContact.Controls.Add(this.btnUpdate);
+            this.newContact.Controls.Add(this.btnDelete);
+            this.newContact.Controls.Add(this.label2);
+            this.newContact.Controls.Add(this.label1);
+            this.newContact.Controls.Add(this.txtMailAddress);
+            this.newContact.Controls.Add(this.txtSurname);
+            this.newContact.Controls.Add(this.lblSurname);
+            this.newContact.Controls.Add(this.txtName);
+            this.newContact.Controls.Add(this.lblName);
+            this.newContact.Controls.Add(this.btnCreate);
+            this.newContact.Controls.Add(this.mskPhonecellNumber);
+            this.newContact.Controls.Add(this.txtId);
+            this.newContact.Controls.Add(this.lblId);
+            this.newContact.Location = new System.Drawing.Point(370, 7);
+            this.newContact.Name = "newContact";
+            this.newContact.Size = new System.Drawing.Size(254, 245);
+            this.newContact.TabIndex = 15;
+            this.newContact.TabStop = false;
+            this.newContact.Text = "New Contact";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(40, 20);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(22, 13);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id :";
+            // 
+            // txtId
+            // 
+            this.txtId.BackColor = System.Drawing.Color.Bisque;
+            this.txtId.Location = new System.Drawing.Point(68, 17);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(156, 20);
+            this.txtId.TabIndex = 15;
+            // 
+            // mskPhonecellNumber
+            // 
+            this.mskPhonecellNumber.BackColor = System.Drawing.Color.Bisque;
+            this.mskPhonecellNumber.Location = new System.Drawing.Point(68, 95);
+            this.mskPhonecellNumber.Mask = "(999) 000-0000";
+            this.mskPhonecellNumber.Name = "mskPhonecellNumber";
+            this.mskPhonecellNumber.Size = new System.Drawing.Size(156, 20);
+            this.mskPhonecellNumber.TabIndex = 3;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(68, 158);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(336, 226);
+            this.dataGridView1.TabIndex = 25;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.Bisque;
+            this.txtName.Location = new System.Drawing.Point(68, 43);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(156, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(21, 46);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 13);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name :";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.BackColor = System.Drawing.Color.Bisque;
+            this.txtSurname.Location = new System.Drawing.Point(68, 69);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(156, 20);
+            this.txtSurname.TabIndex = 2;
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(7, 72);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(55, 13);
+            this.lblSurname.TabIndex = 5;
+            this.lblSurname.Text = "Surname :";
+            // 
+            // txtMailAddress
+            // 
+            this.txtMailAddress.BackColor = System.Drawing.Color.Bisque;
+            this.txtMailAddress.Location = new System.Drawing.Point(68, 121);
+            this.txtMailAddress.Name = "txtMailAddress";
+            this.txtMailAddress.Size = new System.Drawing.Size(156, 20);
+            this.txtMailAddress.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "PhoneCell :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Mail :";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(173, 216);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(149, 158);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(6, 216);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(708, 264);
+            this.Controls.Add(this.newContact);
+            this.Controls.Add(this.Contact);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Contact.ResumeLayout(false);
+            this.newContact.ResumeLayout(false);
+            this.newContact.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Contact;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox newContact;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMailAddress;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.MaskedTextBox mskPhonecellNumber;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Contact/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Contact
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ProjeDemos;Integrated Security=True");
+
+        void listed()
+        {
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("select * from Contacts", connection);
+            dataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+        }
+
+        void clear()
+        {
+            txtId.Text = "";
+            txtName.Text = "";
+            txtSurname.Text = "";
+            txtMailAddress.Text = "";
+            mskPhonecellNumber.Text = "";
+            txtName.Focus();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listed();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("insert into Contacts (Name, Surname, Phonecell, Mail) values (@p1, @p2, @p3, @p4)", connection);
+            command.Parameters.AddWithValue("@p1", txtName.Text);
+            command.Parameters.AddWithValue("@p2", txtSurname.Text);
+            command.Parameters.AddWithValue("@p3", mskPhonecellNumber.Text);
+            command.Parameters.AddWithValue("@p4", txtMailAddress.Text);
+            command.ExecuteNonQuery(); //sorguyu alıştır anlamına gelir.
+
+            connection.Close();
+            MessageBox.Show("The person registered in the system", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listed();
+            clear();
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int select = dataGridView1.SelectedCells[0].RowIndex;
+
+            txtId.Text = dataGridView1.Rows[select].Cells[0].Value.ToString();
+            txtName.Text = dataGridView1.Rows[select].Cells[1].Value.ToString();
+            txtSurname.Text = dataGridView1.Rows[select].Cells[2].Value.ToString();
+            mskPhonecellNumber.Text = dataGridView1.Rows[select].Cells[3].Value.ToString();
+            txtMailAddress.Text = dataGridView1.Rows[select].Cells[4].Value.ToString();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("delete from contacts where Id=" + txtId.Text, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("The person has been deleted.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            listed();
+            clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("update contacts set Name=@p1, Surname=@p2, Phonecell=@p3, Mail=@p4 where Id=@p5", connection);
+            command.Parameters.AddWithValue("@p1", txtName.Text);
+            command.Parameters.AddWithValue("@p2", txtSurname.Text);
+            command.Parameters.AddWithValue("@p3", mskPhonecellNumber.Text);
+            command.Parameters.AddWithValue("@p4", txtMailAddress.Text);
+            command.Parameters.AddWithValue("@p5", txtId.Text);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("The person Info is updated", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            listed();
+            clear();
+
+
+        }
+    }
+}
